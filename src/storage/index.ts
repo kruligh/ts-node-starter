ea9bddf06884c028c4a3cf1b19e92aa159398b57
Dominik Kroliczek <dominik@kroliczek.com>
2018-11-13 00:01:09 +0100

    Login

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,18 +1,2 @@
-import { getData, getSheetsConfig } from "src/storage/spreadsheet";
-import { UserWithPassword } from "./model";
-
 export * from "./model";
-
-const sheetsConfig = getSheetsConfig();
-
-export const getUserWithPassword = async (login: string): Promise<UserWithPassword> => {
-
-  const rawData = await getData(sheetsConfig.usersSheetId, 'A2:E');
-
-  console.log(rawData);
-  return {
-    login,
-    password: "asd",
-  };
-};
-
+export * from "./spreadsheet/query";
--- src/storage/spreadsheet/query.ts
+++ src/storage/spreadsheet/query.ts
@@ -1,7 +1,23 @@
-import { google } from "googleapis";
-import { googleApisAuth } from "src/storage/spreadsheet/config";
+import { google, sheets_v4 } from "googleapis";
+import { UserWithPassword } from "src/storage/model";
+import { getSheetsConfig, googleApisAuth } from "src/storage/spreadsheet/config";
+import Schema$ValueRange = sheets_v4.Schema$ValueRange;
 
-export const getData = async (spreadsheetId: string, range: string) => {
+const sheetsConfig = getSheetsConfig();
+
+export const getUserWithPassword = async (login: string): Promise<UserWithPassword> => {
+  const rawData = await getData(sheetsConfig.usersSheetId, 'A1:C100');
+  const users: UserWithPassword[] = rawData.values.map((rawUser: string[]) => {
+    return {
+      login: rawUser[0],
+      password: rawUser[1],
+    };
+  });
+
+  return users.find((user) => user.login === login);
+};
+
+export const getData = async (spreadsheetId: string, range: string): Promise<Schema$ValueRange> => {
   const auth = await googleApisAuth();
   const sheets = google.sheets({ version: 'v4', auth });
 
@@ -12,7 +28,7 @@ export const getData = async (spreadsheetId: string, range: string) => {
         if (err) {
           reject(err);
         } else {
-          resolve(response);
+          resolve(response.data);
         }
       },
     );
